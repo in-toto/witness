25a1a4f0fc41ab2386463230186734b82b241d7a
Mikhail Swift <mswift@mswift.dev>
2021-12-30 11:30:31 -0500

    chore(ci): update copyright notice
    Updates the copyright notice from "The TestifySec Authors" to "The
    Witness Contributors".  Contributors own the copyright to their
    contributions so this feels like a more accurate way to represent the
    copyright of the project.
    
    Signed-off-by: Mikhail Swift <mswift@mswift.dev>

--- dev/build-and-push-builders.sh
+++ dev/build-and-push-builders.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Copyright 2021 The TestifySec Authors
+# Copyright 2021 The Witness Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
