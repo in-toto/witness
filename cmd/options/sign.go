2cb716cf4085a8c5c469c397b757f3faac2b1056
Cole Kennedy <cole@testifysec.com>
2022-01-12 09:37:40 -0600

    fix: use a flag instead of argument in verify command. BREAKING CHANGE: changes in sign cli.

--- cmd/options/sign.go
+++ cmd/options/sign.go
@@ -20,10 +20,12 @@ type SignOptions struct {
 	KeyOptions  KeyOptions
 	DataType    string
 	OutFilePath string
+	InFilePath  string
 }
 
 func (so *SignOptions) AddFlags(cmd *cobra.Command) {
 	so.KeyOptions.AddFlags(cmd)
 	cmd.Flags().StringVarP(&so.DataType, "datatype", "t", "https://witness.testifysec.com/policy/v0.1", "The URI reference to the type of data being signed. Defaults to the Witness policy type")
 	cmd.Flags().StringVarP(&so.OutFilePath, "outfile", "o", "", "File to write signed data. Defaults to stdout")
+	cmd.Flags().StringVarP(&so.InFilePath, "infile", "f", "", "Witness policy file to sign")
 }
