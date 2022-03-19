ddec111006556b5cdb6935cd5562befa4d204eb9
cole kennedy <colek42@gmail.com>
2022-03-19 16:52:12 -0500

    more files

---
+++ pkg/signer/spire/spire.go
@@ -0,0 +1,47 @@
+// Copyright 2022 The Witness Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package spire
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
+	"github.com/testifysec/witness/pkg/cryptoutil"
+)
+
+type ErrInvalidSVID string
+
+func (e ErrInvalidSVID) Error() string {
+	return fmt.Sprintf("invalid svid: %v", string(e))
+}
+
+func GetSigner(ctx context.Context, socketPath string) (cryptoutil.Signer, error) {
+	svidCtx, err := workloadapi.FetchX509Context(ctx, workloadapi.WithAddr(socketPath))
+	if err != nil {
+		return nil, err
+	}
+
+	svid := svidCtx.DefaultSVID()
+	if len(svid.Certificates) <= 0 {
+		return nil, ErrInvalidSVID("no certificates")
+	}
+
+	if svid.PrivateKey == nil {
+		return nil, ErrInvalidSVID("no private key")
+	}
+
+	return cryptoutil.NewSigner(svid.PrivateKey, cryptoutil.SignWithIntermediates(svid.Certificates[1:]), cryptoutil.SignWithCertificate(svid.Certificates[0]))
+}
