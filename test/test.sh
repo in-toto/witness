137661eaa622241f4c59a52dc52872368060d044
Mikhail Swift <mikhail@testifysec.com>
2022-08-01 12:47:21 -0400

    bug: fix verification fail when policy contains multiple pubkeys
    The DSSE code failed early if a verifier failed to successfully verify
    an envelope. This caused the policy verification code to incorrectly
    report that the policy failed verification.
    
    Signed-off-by: Mikhail Swift <mikhail@testifysec.com>

--- test/test.sh
+++ test/test.sh
@@ -18,14 +18,25 @@ set -e
 DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 . "$DIR/common.sh"
 
-if ! checkprograms make ; then
+if ! checkprograms make tar ; then
   exit 1
 fi
 
-
-
 make -C ../ build
-rm -f ./test-attestation.demo ./testapp ./policy-signed.json
-../bin/witness -c test.yaml run -- go build -o=testapp .
-../bin/witness -c test.yaml sign -f policy.json
+rm -f ./policy-signed.json ./build.attestation.json ./package.attestation.json ./fail.attestation.json ./testapp ./testapp.tar.tgz
+../bin/witness -c test.yaml -l debug sign -f policy.json
+
+# successful test
+../bin/witness -c test.yaml run -o build.attestation.json -- go build -o=testapp .
+../bin/witness -c test.yaml run -s package -k ./testkey2.pem -o package.attestation.json -- tar czf ./testapp.tar.tgz ./testapp
 ../bin/witness -c test.yaml verify
+
+# make sure we fail if we run with a key not in the policy
+../bin/witness -c test.yaml run -k failkey.pem -o ./fail.attestation.json  -- go build -o=testapp .
+../bin/witness -c test.yaml run -s package -k ./testkey2.pem -o package.attestation.json -- tar czf ./testapp.tar.tgz ./testapp
+set +e
+../bin/witness -c test.yaml verify -a ./fail.attestation.json -a ./package.attestation.json
+if [ $? -eq 0 ]; then
+  echo "expected verify to fail"
+  exit 1
+fi
