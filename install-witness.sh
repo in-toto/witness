c4a7449658148d0ba9f77fad7353cacbc3f09218
cole kennedy <colek42@gmail.com>
2023-02-17 16:16:24 -0600

    fix:various fixes and improvments in install script, remove need for sudo and jq, downloads to temp dir, and better hash checking logic

--- install-witness.sh
+++ install-witness.sh
@@ -13,21 +13,39 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-set -e
+#!/bin/bash
+set -eou pipefail
+
+# Create a temporary directory for downloaded files
+TEMPDIR=$(mktemp -d)
+# trap syscalls and delete the temporary directory
+trap "rm -rf $TEMPDIR" EXIT
+
+#install directory should be the first argument or default to /usr/local/bin
+INSTALL_DIR=${1:-"/usr/local/bin"}
+
+# resolve symlinks
+INSTALL_DIR=$(readlink -f $INSTALL_DIR)
+
+# check for bad install directory
+if [ ! -d $INSTALL_DIR ]; then
+    echo "Install directory $INSTALL_DIR does not exist"
+    exit 1
+fi
 
 # Get the latest version of Witness
-VERSION=$(curl -s https://api.github.com/repos/testifysec/witness/releases/latest | jq -r '.tag_name')
+VERSION=$(curl -s https://api.github.com/repos/testifysec/witness/releases/latest | grep -o '"tag_name": *"[^"]*"' | sed 's/"//g' | sed 's/tag_name: *//')
 
 #remove the 'v' from the version
-VERSION=${VERSION:1}
+readonly VERSION=${VERSION:1}
 
 # Determine the architecture of the system
 ARCH=$(uname -m)
 if [ "$ARCH" == "x86_64" ]; then
-    ARCH="amd64"
+    readonly ARCH="amd64"
 fi
 if [ "$ARCH" == "aarch64" ]; then
-    ARCH="arm64"
+    readonly ARCH="arm64"
 fi
 
 ### Determine if Arch is supported
@@ -42,7 +60,14 @@ OS=$(uname -s)
 OS=$(echo "$OS" | tr '[:upper:]' '[:lower:]')
 FILENAME="witness_${VERSION}_${OS}_${ARCH}.tar.gz"
 DOWNLOAD_URL="https://github.com/testifysec/witness/releases/download/v$VERSION/$FILENAME"
-CHECKSUM="$(curl -s  https://github.com/testifysec/witness/releases/download/v$VERSION/witness_${VERSION}_checksums.txt | grep "witness_${VERSION}_${OS}_$ARCH.tar.gz" | awk '{print $1}')"
+EXPECTED_CHECKSUM="$(curl -L -s  https://github.com/testifysec/witness/releases/download/v$VERSION/witness_${VERSION}_checksums.txt | grep -w "witness_${VERSION}_${OS}_$ARCH.tar.gz$" | awk '{print $1}')"
+readonly EXPECTED_CHECKSUM EXPECTED_CHECKSUM
+
+echo "Latest version of Witness is $VERSION"
+echo "Downloading for $OS $ARCH from $DOWNLOAD_URL"
+
+echo expected checksum: "$EXPECTED_CHECKSUM"
+
 
 ## Check to see if OS is supported
 if [ $OS != "linux" ] && [ $OS != "darwin" ]; then
@@ -50,12 +75,16 @@ if [ $OS != "linux" ] && [ $OS != "darwin" ]; then
     exit 1
 fi
 
-# Download the binary
-curl -LO $DOWNLOAD_URL
+# Download the binary to the temporary directory
+cd $TEMPDIR
+curl -s -LO $DOWNLOAD_URL
 
 # Verify the checksum
-echo "$CHECKSUM witness_${VERSION}_${OS}_${ARCH}.tar.gz | sha256sum -c - "
-if [ $? -ne 0 ]; then
+FILE_CHECKSUM=$(sha256sum -b witness_${VERSION}_${OS}_${ARCH}.tar.gz | awk '{print $1}')
+
+echo file checksum: "    $FILE_CHECKSUM"
+
+if [ "$EXPECTED_CHECKSUM" != "$FILE_CHECKSUM" ]; then
     echo "Checksum verification failed, exiting"
     exit 1
 fi
@@ -63,11 +92,28 @@ fi
 # Extract and install the binary
 tar -xzf witness_${VERSION}_${OS}_${ARCH}.tar.gz
 
-# Move the binary to /usr/local/bin
-sudo mv witness /usr/local/bin/
+# Check if the user has write permission for /usr/local/bin/witness
+if [ -w $INSTALL_DIR ]; then
+  # If the user has write permission, move the binary to /usr/local/bin
+  mv witness $INSTALL_DIR
+else
+  # If the user doesn't have write permission, check if the script is running in an interactive terminal
+  if [ -n "$TERM" ]; then
+    # If the script is running in an interactive terminal, prompt for sudo and move the binary to /usr/local/bin
+    echo "You don't have permission to move the file, if you would like to install Witness to ${INSTALL_DIR}, please enter your password."
+    echo "You can also install Witness to a different directory by running this script with the directory as the first argument."
+    sudo -v && sudo mv witness $INSTALL_DIR
+  else
+    # If the script is not running in an interactive terminal, print an error message and exit
+    echo "You don't have permission to move the file, please run with sudo."
+    exit 1
+  fi
+fi
 
-#Remove the tar.gz file
-rm witness_${VERSION}_${OS}_${ARCH}.tar.gz
+# Return to the original directory and remove the temporary directory silently
+cd - > /dev/null
+rm -rf $TEMPDIR
 
 # Check if the binary is installed
 witness version
+echo "Witness v${VERSION} has been installed at ${INSTALL_DIR}/witness"
