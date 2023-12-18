cf706b01342462385e0e8caeb0bcac23e6b6d5cf
Cole <cole@testifysec.com>
2023-12-17 18:38:16 -0600

    [WIP] refactor

--- report/report.go
+++ report/report.go
@@ -166,70 +166,100 @@ func GeneratePDFReport(stepWiseData map[string]StepData, filename string) error 
 	pdf.SetFont("Arial", "", 10)
 
 	for stepName, stepData := range stepWiseData {
-		// Step Header
-		pdf.SetFont("Arial", "B", 12)
-		pdf.CellFormat(0, 10, fmt.Sprintf("Step: %s", stepName), "B", 1, "L", false, 0, "")
-		pdf.Ln(8)
+		renderStepHeader(pdf, stepName)
+		renderTimeFrame(pdf, stepData.StartTime, stepData.EndTime)
 
-		// Time Frame
-		startTime := stepData.StartTime.Format(time.RFC3339)
-		endTime := stepData.EndTime.Format(time.RFC3339)
-		pdf.SetFont("Arial", "I", 10)
-		pdf.CellFormat(0, 10, fmt.Sprintf("Time Frame: %s - %s", startTime, endTime), "", 1, "L", false, 0, "")
-		pdf.Ln(8)
-
-		// Attestations
 		for attestationType, attestationData := range stepData.Data {
-			pdf.SetFont("Arial", "U", 10)
-			pdf.CellFormat(0, 10, attestationType, "", 1, "L", false, 0, "")
-			pdf.Ln(4)
+			renderAttestationHeader(pdf, attestationType)
 
 			if dataMap, ok := attestationData.(map[string]interface{}); ok {
 				for key, value := range dataMap {
-					x, y := pdf.GetXY()
-
-					// Calculate maximum height needed for both key and value cells
-					keyHeight := CalculateRowHeight(pdf, key, keyWidth, lineHeight)
-					valueHeight := CalculateRowHeight(pdf, fmt.Sprintf("%v", value), valueWidth, lineHeight)
-					maxHeight := keyHeight
-					if valueHeight > maxHeight {
-						maxHeight = valueHeight
-					}
-
-					// Check if a new page is needed
-					_, pageHeight := pdf.GetPageSize()
-					if y+maxHeight > pageHeight-bottomMargin {
-						pdf.AddPage()
-						y = pdf.GetY()
-					}
-
-					// Render Key Cell
-					pdf.SetXY(x, y)
-					pdf.MultiCell(keyWidth, lineHeight, key, "1", "L", false)
-
-					// Render Value Cell
-					pdf.SetXY(x+keyWidth, y)
-					pdf.MultiCell(valueWidth, lineHeight, fmt.Sprintf("%v", value), "1", "L", false)
-
-					// Adjust Y position for next row
-					pdf.SetXY(x, y+maxHeight)
+					// Convert value to a string, handling different possible types
+					valueStr := formatValue(value)
+					renderRow(pdf, key, valueStr, keyWidth, valueWidth, lineHeight, bottomMargin)
 				}
 			}
-			pdf.Ln(4)
+			pdf.Ln(4) // Space after each attestation
 		}
-		pdf.Ln(6)
+
+		pdf.Ln(6) // Space after each step
 	}
 
-	err := pdf.OutputFileAndClose(filename)
-	if err != nil {
-		return err
+	return pdf.OutputFileAndClose(filename)
+}
+
+func renderStepHeader(pdf *gofpdf.Fpdf, stepName string) {
+	pdf.SetFont("Arial", "B", 12)
+	pdf.CellFormat(0, 10, fmt.Sprintf("Step: %s", stepName), "B", 1, "L", false, 0, "")
+	pdf.Ln(8)
+}
+
+func renderTimeFrame(pdf *gofpdf.Fpdf, startTime, endTime time.Time) {
+	timeFormat := time.RFC3339
+	pdf.SetFont("Arial", "I", 10)
+	pdf.CellFormat(0, 10, fmt.Sprintf("Time Frame: %s - %s", startTime.Format(timeFormat), endTime.Format(timeFormat)), "", 1, "L", false, 0, "")
+	pdf.Ln(8)
+}
+
+func renderAttestationHeader(pdf *gofpdf.Fpdf, attestationType string) {
+	pdf.SetFont("Arial", "U", 10)
+	pdf.CellFormat(0, 10, attestationType, "", 1, "L", false, 0, "")
+	pdf.Ln(4)
+}
+
+func renderRow(pdf *gofpdf.Fpdf, key, value string, keyWidth, valueWidth, lineHeight, bottomMargin float64) {
+	x, y := pdf.GetXY()
+	_, pageHeight := pdf.GetPageSize()
+	maxHeight := getMaxHeight(pdf, key, value, keyWidth, valueWidth, lineHeight)
+
+	// Add new page if needed
+	if y+maxHeight > pageHeight-bottomMargin {
+		pdf.AddPage()
+		y = pdf.GetY()
 	}
 
-	return nil
+	// Render Key Cell
+	pdf.SetXY(x, y)
+	pdf.MultiCell(keyWidth, lineHeight, key, "1", "L", false)
+
+	// Render Value Cell
+	pdf.SetXY(x+keyWidth, y)
+	pdf.MultiCell(valueWidth, lineHeight, value, "1", "L", false)
+
+	// Adjust Y position for next row
+	pdf.SetXY(x, y+maxHeight)
+}
+
+func getMaxHeight(pdf *gofpdf.Fpdf, key, value string, keyWidth, valueWidth, lineHeight float64) float64 {
+	keyHeight := CalculateRowHeight(pdf, key, keyWidth, lineHeight)
+	valueHeight := CalculateRowHeight(pdf, value, valueWidth, lineHeight)
+	if valueHeight > keyHeight {
+		return valueHeight
+	}
+	return keyHeight
 }
 
 // CalculateRowHeight calculates the required height for a MultiCell.
-func CalculateRowHeight(pdf *gofpdf.Fpdf, text string, width float64, lineHeight float64) float64 {
+func CalculateRowHeight(pdf *gofpdf.Fpdf, text string, width, lineHeight float64) float64 {
 	splitText := pdf.SplitLines([]byte(text), width)
 	return float64(len(splitText)) * lineHeight
+}
+
+func formatValue(value interface{}) string {
+	switch v := value.(type) {
+	case string:
+		return v
+	case float64, float32, int, int32, int64, uint, uint32, uint64:
+		return fmt.Sprintf("%v", v)
+	case []string:
+		return strings.Join(v, ", ")
+	case []interface{}:
+		var strSlice []string
+		for _, item := range v {
+			strSlice = append(strSlice, fmt.Sprintf("%v", item))
+		}
+		return strings.Join(strSlice, " ")
+	default:
+		return fmt.Sprintf("%v", v) // Fallback for other types
+	}
 }
