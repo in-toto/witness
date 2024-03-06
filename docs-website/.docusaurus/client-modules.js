1599e76f29784133de6427f845a24a88abbce80d
John Kjell <john@testifysec.com>
2024-03-06 15:41:10 -0600

    Remove .docusaurus directory from git
    Signed-off-by: John Kjell <john@testifysec.com>

--- docs-website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/jkjell/sandbox/ChaosInTheCRD/witness/docs-website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jkjell/sandbox/ChaosInTheCRD/witness/docs-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jkjell/sandbox/ChaosInTheCRD/witness/docs-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/jkjell/sandbox/ChaosInTheCRD/witness/docs-website/src/css/custom.css"),
-];
