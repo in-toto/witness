79afd67a3d97bbf88ac52f3dd883ec8e258fce05
chaosinthecrd <tom@tmlabs.co.uk>
2024-06-24 16:22:59 -0700

    passing kms options through for use in policy verification
    Signed-off-by: chaosinthecrd <tom@tmlabs.co.uk>

--- cmd/verify.go
+++ cmd/verify.go
@@ -196,6 +196,7 @@ func runVerify(ctx context.Context, vo options.VerifyOptions, verifiers ...crypt
 		witness.VerifyWithPolicyCAIntermediates(policyIntermediates),
 		witness.VerifyWithPolicyCertConstraints(vo.PolicyCommonName, vo.PolicyDNSNames, vo.PolicyEmails, vo.PolicyOrganizations, vo.PolicyURIs),
 		witness.VerifyWithPolicyFulcioCertExtensions(vo.PolicyFulcioCertExtensions),
+		witness.VerifyWithKMSProviderOptions(vo.KMSVerifierProviderOptions),
 	)
 	if err != nil {
 		if verifiedEvidence.StepResults != nil {
